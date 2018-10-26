d7d2f0ddb17170e84a63fbe0ff5a3f42769a8ca9
Anu Khambete <anukhambete@gmail.com>
2018-10-26 03:09:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
      resources :sessions
      resources :secrets
-
+     root 'sessions#new'
      post '/logout' => 'sessions#destroy'
 
      get '/show' => 'secrets#show'
